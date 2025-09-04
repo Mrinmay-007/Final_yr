ec425397f95f63a95a42502e764943f89e88e206
mrinmay2004 <mrinmay21012204@gmail.com>
2025-09-04 21:58:54 +0530

    first commit

--- api/main.py
+++ api/main.py
@@ -1,13 +1,11 @@
 
 
-from fastapi import FastAPI, File, UploadFile #type: ignore
+from fastapi import FastAPI  #type: ignore
 from fastapi.middleware.cors import CORSMiddleware #type: ignore
 import uvicorn #type: ignore
-import numpy as np
-from io import BytesIO
-from PIL import Image  #type: ignore
-import tensorflow as tf  #type: ignore
 import os
+
+from api.routers import prediction
 
 app = FastAPI()
 
@@ -20,54 +18,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# Path to your model (use .keras or .h5 format)
-MODEL_PATH= "D:\\USER\\OneDrive\\Desktop\\Final_yr\\models\\V1.keras" # Update this path
-
-# MODEL_PATH = "../models/V1.keras"  # or "../models/1.h5"
-
-# Load the model depending on extension
-if MODEL_PATH.endswith(".keras") or MODEL_PATH.endswith(".h5"):
-    MODEL = tf.keras.models.load_model(MODEL_PATH)
-else:
-    # Fallback for TensorFlow SavedModel directory
-    MODEL = tf.keras.layers.TFSMLayer(MODEL_PATH, call_endpoint="serving_default")
-
-# Class names
-CLASS_NAMES = ["Early Blight", "Late Blight", "Healthy"]
+# ======================
+#  API Router
+# ======================
 
 
-@app.get("/ping")
-async def ping():
-    return {"message": "Hello, World!"}
+app.include_router(prediction.router)
 
-
-def read_file_as_image(data) -> np.ndarray:
-    """Convert uploaded image bytes into a numpy array"""
-    image = Image.open(BytesIO(data)).convert("RGB")
-    image = image.resize((256, 256))  # resize if your model expects fixed size
-    return np.array(image)
-
-
-@app.post("/predict")
-async def predict(file: UploadFile = File(...)):
-    # Read the uploaded image
-    image = read_file_as_image(await file.read())
-
-    # Expand dimensions to create batch of size 1
-    img_batch = np.expand_dims(image, 0)
-
-    # Run prediction
-    predictions = MODEL.predict(img_batch)
-
-    # Get class & confidence
-    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
-    confidence = float(np.max(predictions[0]))
-
-    return {
-        "class": predicted_class,
-        "confidence": confidence
-    }
-
+# ======================
+# Main
+# ======================
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
