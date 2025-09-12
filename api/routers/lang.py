84f019ee286f8d0999244f389ed73b9ebce339ba
mrinmay2004 <mrinmay21012204@gmail.com>
2025-09-12 12:17:09 +0530

    first commit

--- api/routers/lang.py
+++ api/routers/lang.py
@@ -2,3 +2,6 @@ from fastapi import APIRouter #type: ignore
 
 
 router = APIRouter()
+# Google Cloud Translate docs. 
+# BHASHINI official site, docs and GitBook. 
+# AI4Bharat (IndicTrans2 / IndicBART) & Hugging Face models. 
