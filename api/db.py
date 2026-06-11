16e4b86fbac7eaf27b6f27a267fda9b6060cf092
mrinmay2004 <mrinmay21012204@gmail.com>
2026-06-11 12:16:12 +0530

    update

--- api/db.py
+++ api/db.py
@@ -1,8 +1,12 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from fastapi import Depends
 from typing import Optional
+from dotenv import load_dotenv
 
-MONGO_URL = "mongodb://localhost:27017/"
+load_dotenv()
+
+MONGO_URL = os.getenv("MONGO_URL")
 DATABASE_NAME = "Project"
 
 
